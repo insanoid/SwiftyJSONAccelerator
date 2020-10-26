c0ce13e5919ffc38c0ac7b80c90535d381d5d727
vvveiii <cntrump@gmail.com>
2020-10-26 21:22:45 +0800

    Update TextView color and font

--- SwiftyJSONAccelerator/UI/SJTextView.swift
+++ SwiftyJSONAccelerator/UI/SJTextView.swift
@@ -8,6 +8,17 @@
 
 import Cocoa
 
+extension NSColor {
+ 
+    @inline(__always) public convenience init(RGB hex: UInt32, alpha: CGFloat = 1) {
+        let red = CGFloat((hex & 0xff0000) >> 16) / 255.0
+        let green = CGFloat((hex & 0xff00) >> 8) / 255.0
+        let blue = CGFloat(hex & 0xff) / 255.0
+        
+        self.init(deviceRed: red, green: green, blue: blue, alpha: alpha)
+    }
+}
+
 /// A textview customization to handle formatting and handling removal of quotes.
 class SJTextView: NSTextView {
     override init(frame frameRect: NSRect, textContainer container: NSTextContainer?) {
@@ -25,8 +36,11 @@ class SJTextView: NSTextView {
     }
 
     internal func updateFormat() {
-        textStorage?.font = NSFont(name: "Menlo", size: 12)
-        textColor = NSColor.white
+        textStorage?.font = NSFont(name: "Monaco", size: 12)
+        
+        let color = NSColor(RGB: 0x07c160)
+        insertionPointColor = color
+        selectedTextAttributes = [.backgroundColor: color.withAlphaComponent(0.2)]
     }
 
     override func paste(_ sender: Any?) {
